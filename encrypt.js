ac91cc80e51ba2ff1019d8c554319dfbcd6af219
Torbjorn Lundberg <torbjorn.lundberg@softhouse.se>
2015-09-24 22:42:54 +0200

    Encryption helper fetches nonce

--- encrypt.js
+++ encrypt.js
@@ -2,13 +2,29 @@
 
 var readline = require('readline');
 var Tea = require('./xxtea.js');
+var http = require('http');
 
 var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-rl.question("Please enter the nonce: ", function (nonce) {
+console.log('Please wait while fetching the next nonce...');
+
+http.get('http://localhost:8080/nonce', function(res) {
+    var data = '';
+
+    res.on('data', function(chunk) {
+        data += chunk;
+    });
+
+    res.on('end', function() {
+        console.log('nonce', data);
+        askForDetails(null, data);
+    });
+});
+
+function askForDetails(err, nonce) {
     rl.question("Please enter your password [password]: ", function (password) {
         rl.question("Please enter your message: ", function (message) {
             rl.close();
@@ -19,4 +35,4 @@ rl.question("Please enter the nonce: ", function (nonce) {
             console.log('encrypted', encodeURIComponent(Tea.encrypt(stringToEncrypt, password)));
         });
     });
-});
+}
